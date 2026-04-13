eab76ce27de203deb8683662ece8a4ac653bfa8e
Atul-Raj7 <meatulraj7@gmail.com>
2026-04-13 10:04:09 +0530

    added timestamp record

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import writeFile from './functions/writeTask.js';
 import { Command } from 'commander';
 const expense = new Command();
 
+const timeStamp = new Date().toString().split("GMT")[0].trim()
+
 function getNextId(data) {
     const ids = data.map(item => item.id)
     let nextId = 0
@@ -24,7 +26,8 @@ expense
         const newExpense = {
             id: getNextId(expenses),
             description: options.description,
-            amount: Number(options.amount)
+            amount: Number(options.amount),
+            date: timeStamp
         }
         expenses.push(newExpense)
         writeFile(expenses)
