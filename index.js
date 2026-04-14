50a507fa1abb829b1ce6d7343ddad6c61154d289
Atul-Raj7 <meatulraj7@gmail.com>
2026-04-14 21:29:07 +0530

    refactoring

--- index.js
+++ index.js
@@ -1,37 +1,16 @@
-import readFile from './functions/readFile.js';
-import writeFile from './functions/writeTask.js';
+import add from './commands/add.js';
+import update from './commands/update.js';
+
+
 import { Command } from 'commander';
 const expense = new Command();
-
-const timeStamp = new Date().toString().split("GMT")[0].trim()
-
-function getNextId(data) {
-    const ids = data.map(item => item.id)
-    let nextId = 0
-    for(const id of ids){
-        if(id !== nextId){
-            return nextId
-        }
-        nextId++
-    }
-    return nextId
-}
 
 expense
     .command('add')
     .option('-d,--description [description]','expense description')
     .option('-a,--amount <amount_spent>','money spent')
     .action((options) => {
-        const expenses = readFile()
-        const newExpense = {
-            id: getNextId(expenses),
-            description: options.description,
-            amount: Number(options.amount),
-            date: timeStamp
-        }
-        expenses.push(newExpense)
-        writeFile(expenses)
-        console.log(`Expense added successfully ID:${newExpense.id}`)
+        add(options)
     })
 
 expense
@@ -40,15 +19,7 @@ expense
     .option('-a,--amount <amount_spent>','money spent')
     .option('-d,--description [description]','expense description')
     .action((options) => {
-        const expenses = readFile()
-        const updateExpense = expenses.find(item => item.id === Number(options.id))
-        if(updateExpense){
-            updateExpense.description = options.description,
-            updateExpense.amount = Number(options.amount),
-            updateExpense.date = timeStamp
-        }
-        writeFile(expenses)
-        console.log(`Expense record ID:${options.id} updated \n\t ${JSON.stringify(updateExpense, null, 2)}`)
+        update(options)
     })
 
 expense.parse()
